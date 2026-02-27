e9c76d63e0a4dfbc017fc6babdc2df22ce8e1cff
ShotsMan2 <uysalselimefe@gmail.com>
2026-02-27 12:31:31 +0300

    refactor: remove unused ikili function

--- bitwise-operators-02/decimal_to_binary_math.c
+++ bitwise-operators-02/decimal_to_binary_math.c
@@ -2,33 +2,6 @@
 #include <stdlib.h>
 #include <time.h>
 
-void ikili(int sayi)
-{
-    int dizi[32];
-    int i = 0, j;
-
-    if (sayi == 0)
-    {
-        printf("0");
-        return;
-    }
-
-    while (sayi > 0)
-    {
-        dizi[i] = sayi & 1;
-
-        sayi = sayi >> 1;
-
-        i++;
-    }
-
-    for (j = i - 1; j >= 0; j--)
-    {
-        printf("%d", dizi[j]);
-    }
-    printf("\n");
-}
-
 int main()
 {
     int sayi, bit;
