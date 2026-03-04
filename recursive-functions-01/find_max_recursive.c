c58591a1302f8f0b422c71e6da027e5cf95ac9b6
Selim Efe <uysalselimefe@gmail.com>
2026-03-04 21:15:59 +0300

    Rename function and variables for clarity

--- recursive-functions-01/find_max_recursive.c
+++ recursive-functions-01/find_max_recursive.c
@@ -2,32 +2,32 @@
 #include <stdlib.h>
 #include <time.h>
 
-int enbuyukbul(int arr[], int size)
+int findMax(int arr[], int size)
 {
     if (size == 1)
     {
         return arr[0];
     }
 
-    int kalanenbuyuk = enbuyukbul(arr, size - 1);
+    int maxOfRest = findMax(arr, size - 1);
 
-    if (kalanenbuyuk < arr[size - 1])
+    if (maxOfRest < arr[size - 1])
     {
         return arr[size - 1];
     }
     else
     {
-        return kalanenbuyuk;
+        return maxOfRest;
     }
 }
 
 int main()
 {
-    int sayilar[10] = {12, 45, 7, 89, 23, 56, 91, 34, 67, 5};
+    int numbers[10] = {12, 45, 7, 89, 23, 56, 91, 34, 67, 5};
 
     int size = 10;
 
-    int result = enbuyukbul(sayilar, size);
+    int result = findMax(numbers, size);
 
     printf("%d", result);
 
