51e009a29ee08b69416f8f9159aa48d04b2bc42b
Selim Efe <uysalselimefe@gmail.com>
2026-03-04 20:24:34 +0300

    Refactor ones complement function and variable names

--- bitwise-operators-02/recursive_ones_complement.c
+++ bitwise-operators-02/recursive_ones_complement.c
@@ -2,48 +2,48 @@
 #include <stdlib.h>
 #include <time.h>
 
-void cevir(int sayi, int i, int dizi[])
+void cevir(int num, int i, int arr[])
 {
     int j;
 
-    if (sayi == 0)
+    if (num == 0)
     {
         for (j = i - 1; j >= 0; j--)
         {
-            printf("%d", dizi[j]);
+            printf("%d", arr[j]);
         }
         printf("\n");
         return;
     }
 
-    dizi[i] = sayi & 1;
+    arr[i] = num & 1;
 
-    if (dizi[i] == 0)
+    if (arr[i] == 0)
     {
-        dizi[i] = 1;
+        arr[i] = 1;
     }
     else
     {
-        dizi[i] = 0;
+        arr[i] = 0;
     }
 
-    cevir(sayi >> 1, i + 1, dizi);
+    cevir(num >> 1, i + 1, arr);
 }
 
 int main()
 {
-    int sayi, arr[32];
+    int num, arr[32];
 
-    printf("sayi gir ");
-    scanf("%d", &sayi);
+    printf("enter a number ");
+    scanf("%d", &num);
 
-    if (sayi == 0)
+    if (num == 0)
     {
         printf("1\n");
     }
     else
     {
-        cevir(sayi, 0, arr);
+        cevir(num, 0, arr);
     }
 
     return 0;
