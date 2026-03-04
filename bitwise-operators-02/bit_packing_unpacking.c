715eea10d2eced11224f1b34dd531fc5b63b5b7b
Selim Efe <uysalselimefe@gmail.com>
2026-03-04 18:52:46 +0300

    Rename variables for consistency in bit packing function

--- bitwise-operators-02/bit_packing_unpacking.c
+++ bitwise-operators-02/bit_packing_unpacking.c
@@ -2,27 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 
-void cevir(int sayi1, int sayi2)
+void cevir(int num1, int num2)
 {
     int data;
 
-    data = sayi2 | sayi1 << 8;
+    data = num2 | num1 << 8;
 
     printf("%d\n", data);
 
-    int cozulensayi2 = data & 0xFF;
+    int cozulennum2 = data & 0xFF;
 
-    int cozulensayi1 = (data >> 8) & 0xFF;
+    int cozulennum1 = (data >> 8) & 0xFF;
 
-    printf("sayi1= %d\n", cozulensayi1);
-    printf("sayi2= %d\n", cozulensayi2);
+    printf("num1= %d\n", cozulennum1);
+    printf("num2= %d\n", cozulennum2);
 }
 
 int main()
 {
-    int sayi1 = 5, sayi2 = 7;
+    int num1 = 5, num2 = 7;
 
-    cevir(sayi1, sayi2);
+    cevir(num1, num2);
 
     return 0;
 }
