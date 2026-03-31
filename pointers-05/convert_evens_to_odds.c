231407f28c5e38b14dfbf93dec321851a9c1885d
ShotsMan2 <uysalselimefe@gmail.com>
2026-03-31 11:03:56 +0300

    feat: implement in-place array manipulation to increment even numbers using shorthand operators and pointer arithmetic

---
+++ pointers-05/convert_evens_to_odds.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+int main()
+{
+    int i, sayilar[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
+    int size = sizeof(sayilar) / sizeof(sayilar[0]);
+
+    for (i = 0; i < size; i++)
+    {
+        if (*(sayilar + i) % 2 == 0)
+        {
+            *(sayilar + i) = *(sayilar + i) + 1;
+        }
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        printf("%d\n", *(sayilar + i));
+    }
+
+    return 0;
+}
