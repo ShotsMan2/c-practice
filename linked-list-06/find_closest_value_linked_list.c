5d30a4d9606284d50bc6dc5182483b8a52eaa35f
ShotsMan2 <uysalselimefe@gmail.com>
2026-04-16 19:42:39 +0300

    fix: resolve uninitialized difference variable and implement abs() for finding closest value in linked list

---
+++ linked-list-06/find_closest_value_linked_list.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+int main()
+{
+    int num, sayi, fark, minfark, count = 1;
+
+    node *head = NULL;
+    node *tail = NULL;
+
+    for (;;)
+    {
+        printf("veri gir ");
+        scanf("%d", &num);
+
+        if (num == -1)
+        {
+            break;
+        }
+
+        node *n = (node *)malloc(sizeof(node));
+        n->data = num;
+        n->next = NULL;
+
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+    }
+
+    printf("sayi gir ");
+    scanf("%d", &sayi);
+
+    node *temp = head;
+
+    minfark = sayi - head->data;
+    if (minfark < 0)
+    {
+        minfark = -minfark;
+    }
+
+    int close = head->data;
+    int sira = count;
+
+    while (temp != NULL)
+    {
+        fark = sayi - temp->data;
+        if (fark < 0)
+        {
+            fark = -fark;
+        }
+
+        if (fark < minfark)
+        {
+            close = temp->data;
+            sira = count;
+            minfark = fark;
+        }
+
+        count++;
+        temp = temp->next;
+    }
+
+    printf("%d sayisi %d sayisina yakindir ve %d. siradadir.", sayi, close, sira);
+
+    return 0;
+}
