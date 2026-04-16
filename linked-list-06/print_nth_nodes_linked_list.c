dd89afd1d4e2e56ff047a58558a987a3fd058f11
ShotsMan2 <uysalselimefe@gmail.com>
2026-04-16 19:02:06 +0300

    feat: implement step traversal algorithm to print every Nth node in a linked list with input validation and memory cleanup

---
+++ linked-list-06/print_nth_nodes_linked_list.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+int main()
+{
+    int num, count = 1, artis;
+
+    node *head = NULL;
+    node *tail = NULL;
+
+    for (;;)
+    {
+        printf("veri gir ");
+        scanf("%d", &num);
+
+        if (num == -1)
+        {
+            break;
+        }
+
+        node *n = (node *)malloc(sizeof(node));
+        n->data = num;
+        n->next = NULL;
+
+        if (head == NULL)
+        {
+            head = n;
+            tail = n;
+        }
+        else
+        {
+            tail->next = n;
+            tail = n;
+        }
+    }
+
+    printf("artis gir ");
+    scanf("%d", &artis);
+
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        if (count == artis)
+        {
+            printf("%d ", temp->data);
+            count = 0;
+        }
+
+        count++;
+
+        temp = temp->next;
+    }
+
+    return 0;
+}
