ad619fdcc74b141d2689d2040841f61446eaacc3
Selim Efe <uysalselimefe@gmail.com>
2026-04-28 11:09:19 +0300

    Rename variable 'sayilar' to 'numbers' in code

--- pointers-05/convert_evens_to_odds.c
+++ pointers-05/convert_evens_to_odds.c
@@ -6,20 +6,20 @@
 
 int main()
 {
-    int i, sayilar[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
-    int size = sizeof(sayilar) / sizeof(sayilar[0]);
+    int i, numbers[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
 
     for (i = 0; i < size; i++)
     {
-        if (*(sayilar + i) % 2 == 0)
+        if (*(numbers + i) % 2 == 0)
         {
-            (*(sayilar + i))++;
+            (*(numbers + i))++;
         }
     }
 
     for (i = 0; i < size; i++)
     {
-        printf("%d\n", *(sayilar + i));
+        printf("%d\n", *(numbers + i));
     }
 
     return 0;
