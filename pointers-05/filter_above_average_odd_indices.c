254e3c5aa273b695bb02863760fe15448b2f6e98
Selim Efe <uysalselimefe@gmail.com>
2026-05-02 12:31:34 +0300

    Rename array from 'sayilar' to 'numbers'

--- pointers-05/filter_above_average_odd_indices.c
+++ pointers-05/filter_above_average_odd_indices.c
@@ -6,12 +6,12 @@
 
 int main()
 {
-    int i, ort, sum = 0, count = 0, sayilar[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
-    int size = sizeof(sayilar) / sizeof(sayilar[0]);
+    int i, ort, sum = 0, count = 0, numbers[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
 
     for (i = 1; i < size; i = i + 2)
     {
-        sum += *(sayilar + i);
+        sum += *(numbers + i);
         count++;
     }
 
@@ -19,9 +19,9 @@ int main()
 
     for (i = 1; i < size; i = i + 2)
     {
-        if (ort < *(sayilar + i))
+        if (ort < *(numbers + i))
         {
-            printf("%d\n", *(sayilar + i));
+            printf("%d\n", *(numbers + i));
         }
     }
 
