5411d23bbdd69443fb7755881169f8aeaa21216c
Selim Efe <uysalselimefe@gmail.com>
2026-05-03 13:04:43 +0300

    Rename variable 'ort' to 'avg' for clarity

--- pointers-05/filter_above_average_odd_indices.c
+++ pointers-05/filter_above_average_odd_indices.c
@@ -6,7 +6,7 @@
 
 int main()
 {
-    int i, ort, sum = 0, count = 0, numbers[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
+    int i, avg, sum = 0, count = 0, numbers[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
     int size = sizeof(numbers) / sizeof(numbers[0]);
 
     for (i = 1; i < size; i = i + 2)
@@ -15,11 +15,11 @@ int main()
         count++;
     }
 
-    ort = sum / count;
+    avg = sum / count;
 
     for (i = 1; i < size; i = i + 2)
     {
-        if (ort < *(numbers + i))
+        if (avg < *(numbers + i))
         {
             printf("%d\n", *(numbers + i));
         }
