89c1bf848fa2a37a315a3550a8a5bd8238e7e2a8
Selim Efe <uysalselimefe@gmail.com>
2026-05-04 22:40:59 +0300

    Rename array 'sayilar' to 'numbers'

--- pointers-05/linear_search_pointer.c
+++ pointers-05/linear_search_pointer.c
@@ -6,14 +6,14 @@
 
 int main()
 {
-    int i, bulundu = 0, num, sayilar[14] = {10, 15, 20, 33, 22, 11, 9, 8, 48, 47, 12, 25, 12, 21};
+    int i, bulundu = 0, num, numbers[14] = {10, 15, 20, 33, 22, 11, 9, 8, 48, 47, 12, 25, 12, 21};
 
     printf("sayi gir ");
     scanf("%d", &num);
 
     for (i = 0; i < 14; i++)
     {
-        if (*(sayilar + i) == num)
+        if (*(numbers + i) == num)
         {
             printf("%d.\n", i + 1);
             bulundu = 1;
