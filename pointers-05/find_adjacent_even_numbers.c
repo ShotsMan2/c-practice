f2f4ed61fc9538b3d201388064345fa800763685
Selim Efe <uysalselimefe@gmail.com>
2026-05-07 12:52:46 +0300

    Rename array and variable for clarity

--- pointers-05/find_adjacent_even_numbers.c
+++ pointers-05/find_adjacent_even_numbers.c
@@ -6,15 +6,15 @@
 
 int main()
 {
-    int i, count = 0, sayilar[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
-    int size = sizeof(sayilar) / sizeof(sayilar[0]);
+    int i, count = 0, numbers[] = {20, 60, 45, 42, 23, 24, 26, 125, 66, 55, 145, 50, 30, 40};
+    int size = sizeof(numbers) / sizeof(numbers[0]);
 
     for (i = 0; i < size - 1; i++)
     {
-        if (*(sayilar + i) % 2 == 0 && *(sayilar + i + 1) % 2 == 0)
+        if (*(numbers + i) % 2 == 0 && *(numbers + i + 1) % 2 == 0)
         {
             count++;
-            printf("%d-%d\n", *(sayilar + i), *(sayilar + i + 1));
+            printf("%d-%d\n", *(numbers + i), *(numbers + i + 1));
         }
     }
 
