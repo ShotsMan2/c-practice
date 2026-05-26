2fa3bb192ef807cae8ac933d7f9e45e6c2617a28
ShotsMan2 <uysalselimefe@gmail.com>
2026-05-26 16:57:12 +0300

    feat: implement algorithm to convert odd numbers to even in a descending sorted linked list while maintaining sort order and memory safety

---
+++ Circular-Linked-List-07/convert_odds_to_evens_linked_list.c
@@ -0,0 +1,93 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} node;
+
+node *head = NULL;
+
+void siraliekle(int num)
+{
+    node *n = (node *)malloc(sizeof(node));
+    n->data = num;
+    n->next = NULL;
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    if (head->data <= num)
+    {
+        n->next = head;
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL && temp->next->data > num)
+    {
+        temp = temp->next;
+    }
+
+    n->next = temp->next;
+    temp->next = n;
+}
+
+void listele()
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        printf("%d ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("\n");
+}
+
+void ciftecevir()
+{
+    node *temp = head;
+    while (temp != NULL)
+    {
+        if ((temp->data) % 2 != 0)
+        {
+            temp->data = temp->data + 1;
+        }
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    int num;
+
+    for (;;)
+    {
+        printf("sayi gir ");
+        scanf("%d", &num);
+
+        if (num == -1)
+        {
+            break;
+        }
+
+        siraliekle(num);
+
+        listele();
+    }
+
+    ciftecevir();
+
+    listele();
+
+    return 0;
+}
